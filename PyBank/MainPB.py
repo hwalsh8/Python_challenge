bd744bb07d3b6d9e8c0a14738914d5fd2b217367
hwalsh8 <heloisaw8@gmail.com>
2019-07-21 14:44:31 -0400

    pybank print update

--- PyBank/MainPB.py
+++ PyBank/MainPB.py
@@ -7,45 +7,43 @@ total_months = 0
 sum_revenue = 0
 revenue_change = 0
 
-#create file path
+#file to read and file to output
 budgetdata = os.path.join("Resources", "budget_data.csv")
+file_to_output = os.path.join("analysis.txt")
 
 #assign data to variables
 with open(budgetdata) as pybank_data:
     reader = csv.reader(pybank_data)
     header = next(reader)
+    # print(header)
 
     #create loop statement for calulations
     for current_row in reader:
-        total_months = total_months + 1     
+        total_months = total_months + 1 
+        # print(total_months)    
         #sum up revenue
         sum_revenue = sum_revenue + (int(current_row[1]))
+
         #revenue = float(current_row[1]) <-output is the last row :(
-        #print(type(current_row[1])) <-is this type str or int
+        #print(type(current_row[1])) 
 
   
-        
+    # print(total_months)
+    # print(sum_revenue)        
 
-#printing outputs for file
-print("Financial Analysis")
-print("-------------------------")
-print("Total Months: " + str(total_months))
-print("Total Revenue: " + "$"+str(sum_revenue))
-#print("Average Change: "("enter variable"))
-#print("Greatest Increase in Profits: "("enter variable"))
-#print("Greatest Decrease in Profits: "("enter variable"))
 
 #creating variable to print output to file and print screen test
-Output = (   
-f("Financial Analysis: ")
-f("-------------------------")
-f("Total Months: " + str(total_months)")
-f("Total Revenue: " + "$"+str(sum_revenue)"))
-print(Output)
+output = (   
+f"\nFinancial Analysis:\n"
+f"-------------------------\n"
+f"Total Months: {total_months}\n"
+f"Total Revenue: ${sum_revenue}\n"
+f"-------------------------\n")
+print(output)
 
-outputtxt = os.path.join('PyBank/TestSolutions.txt')
-with open(outputtxt, 'w') as txtfile:
-    txtwriter = txtfile.write(Output)
+outputtxt = os.path.join('PyBank/analysis.txt')
+with open(file_to_output, 'w') as txt_file:
+    txt_file.write(output)
 
 print("end session")
 
